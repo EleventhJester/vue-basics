2eec4d856d19108893a523826c060804e8a697f7
11 <leeroycunha@gmail.com>
2023-08-17 14:56:56 +0200

    main.js fot createApp data

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+        data() {
+            return {
+                product: 'Socks',
+                description: 'The warmest socks you\'ll ever wear!',
+                image: './assets/images/socks_blue.jpg',
+                url: 'https://www.vuemastery.com/',
+                inventory: 1111,
+                onSale: true,
+                details: ['50% cotton','30% wool','20% polyester'],
+                sizes: ['S', 'M', 'L', 'XL'],
+                variants: [
+                    { id: 2234, color: 'green' },
+                    { id: 2235, color: 'blue' },
+                ]
+            }
+        }
+})
